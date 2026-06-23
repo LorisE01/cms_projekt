bccc3708060edffa57c4e551e5135f031cbf8586
Loris Emmerich <loris.emmerich@iem.thm.de>
2026-06-23 17:32:54 +0200

    reversed changes in single.php & deleted single.css

--- single.php
+++ single.php
@@ -1,23 +1,23 @@
 <?php get_header(); // Lädt die Navigation und den Header ?>
 
-<div class="single-person-page container py-5">
+<div class="container py-5" style="margin-top: 100px;">
     <?php while ( have_posts() ) : the_post(); ?>
         <div class="row">
             <!-- Das Bild der Person -->
             <div class="col-md-4 text-center">
                 <?php if ( has_post_thumbnail() ) { 
-                    the_post_thumbnail('large', array('class' => 'single-person-image img-fluid rounded-circle'));
+                    the_post_thumbnail('large', array('class' => 'img-fluid rounded-circle', 'style' => 'width: 300px; height: 300px; object-fit: cover;')); 
                 } ?>
             </div>
 
             <!-- Der Inhalt (Name, Text, Links) -->
             <div class="col-md-8">
-                <h1 class="single-person-title text-uppercase"><?php the_title(); ?></h1>
+                <h1 class="text-uppercase" style="color: #4a5e65;"><?php the_title(); ?></h1>
                 <hr>
                 <div class="mt-4">
                     <?php the_content(); ?>
                 </div>
-                <a href="<?php echo home_url(); ?>#lehrende" class="single-person-back btn mt-4">Zurück zur Übersicht</a>
+                <a href="<?php echo home_url(); ?>#lehrende" class="btn btn-secondary mt-4" style="background-color: #4a5e65;">Zurück zur Übersicht</a>
             </div>
         </div>
     <?php endwhile; ?>
