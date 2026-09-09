02421a5c612c75b493f4bff7601dca7a93030d99
Loris Emmerich <loris.emmerich@iem.thm.de>
2026-09-09 19:18:33 +0200

    Added WooCommerce

--- functions.php
+++ functions.php
@@ -75,4 +75,20 @@ function agency_add_a_class($atts, $item, $args) {
 }
 add_filter('nav_menu_link_attributes', 'agency_add_a_class', 1, 3);
 
+
+// 4. WooCommerce Unterstützung aktivieren
+function thm_theme_woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'thm_theme_woocommerce_support' );
+
+// 5. WooCommerce Produktgalerie Unterstützung aktivieren
+function thm_theme_woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+    add_theme_support( 'wc-product-gallery-zoom' );
+    add_theme_support( 'wc-product-gallery-lightbox' );
+    add_theme_support( 'wc-product-gallery-slider' );
+}
+add_action( 'after_setup_theme', 'thm_theme_woocommerce_support' );
+
 ?>
